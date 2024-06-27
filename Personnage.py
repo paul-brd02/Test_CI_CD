d92d1f74ace0b02975901583aded03cc9141a83b
paul-brd02 <paul.bardou@live.fr>
2024-06-27 16:20:24 +0200

    Init

---
+++ Personnage.py
@@ -0,0 +1,37 @@
+class Personnage():
+    def __init__(self) -> None:
+        self.__point_de_vie = 100
+        self.__point_attaque = 50
+        self.__est_mort = False
+
+    def get_point_de_vie(self):
+        return self.__point_de_vie
+    
+    def get_point_attaque(self):
+        return self.__point_attaque
+    
+    def get_est_mort(self):
+        return self.__est_mort
+    
+    def set_point_de_vie(self, value: int):
+        self.__point_de_vie = value
+    
+    def set_point_attaque(self, value: int):
+        self.__point_attaque = value
+    
+    def set_est_mort(self):
+        self.__est_mort = True
+
+    def tuer(self):
+        self.set_point_de_vie(0)
+        self.set_est_mort()
+
+    def reduire_point_de_vie(self, value: int):
+        self.set_point_de_vie(self.__point_de_vie - value)
+
+    def attaque(self, personnage:'Personnage'):
+        attaque = self.get_point_attaque()
+        if attaque > personnage.get_point_de_vie():
+            personnage.set_point_de_vie(0)
+        else:
+            personnage.reduire_point_de_vie(attaque)
---
+++ tests/test.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from Personnage import Personnage
+
+class TestPersonnage(TestCase):
+
+    def test_point_de_vie(self):
+        personnage = Personnage()
+        self.assertEqual(100, personnage.get_point_de_vie())
+    
+    def test_tuer(self):
+        personnage = Personnage()
+        personnage.tuer()
+        self.assertTrue(personnage.get_est_mort())
+        self.assertEqual(0, personnage.get_point_de_vie())
+
+    def test_vivant(self):
+        personnage = Personnage()
+        self.assertFalse(personnage.get_est_mort())
+
+    def test_reduire_point_de_vie(self):
+        personnage = Personnage()
+        personnage.reduire_point_de_vie(50)
+        self.assertEqual(50, personnage.get_point_de_vie())
+
+    def test_attaque(self):
+        personnage1 = Personnage()
+        personnage2 = Personnage()
+        personnage2_point_de_vie = personnage2.get_point_de_vie()
+        personnage1.attaque(personnage2)
+        self.assertNotEqual(personnage2_point_de_vie, personnage2.get_point_de_vie())
+
+    def test_point_attaque(self):
+        personnage = Personnage()
+        self.assertEqual(50, personnage.get_point_attaque())
